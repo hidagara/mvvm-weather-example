fe4c50a3f9e60d60dc0c5d1c9cc272b4935608bb
hidagara <hidagara@gmail.com>
2019-12-02 22:05:57 +0300

    Initial Commit

---
+++ mvvm-weather-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm-weather-example
+//
+//  Created by Roman Guseynov on 02.12.2019.
+//  Copyright © 2019 Roman Guseynov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
